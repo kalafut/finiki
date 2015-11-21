c5ea367731921281ecad7f01471d2c510cb190a3
Jim Kalafut <jim@kalafut.net>
2015-11-20 16:08:13 -0800

    Cleanup

--- src/finiki/main.go
+++ src/finiki/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -11,17 +10,9 @@ import (
 var config = readLocalCfg()
 var siteConfig = readSiteCfg()
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-
 func main() {
-	//storage := NewDumbStorage()
 	storage := NewFlatFileStorage(config.DataLocation)
 	w := NewWiki(storage)
-	//f := NewFolder("sample")
-
-	//printFolder(f, "")
 
 	router := httprouter.New()
 	router.GET("/show/*path", w.Show)
@@ -29,14 +20,4 @@ func main() {
 	router.POST("/edit/*path", w.Update)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func printFolder(f Folder, parent string) {
-	for name, page := range f.pages {
-		fmt.Printf("%s - %s\n", parent+name, (*page).String())
-	}
-
-	for name, folder := range f.folders {
-		printFolder(folder, parent+name+"/")
-	}
 }
