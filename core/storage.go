262f1dbeb9ea501d883e28ce66f987d0976b2d7c
Jim Kalafut <jim@kalafut.net>
2015-12-03 14:35:53 -0800

    Better path splitting

--- core/storage.go
+++ core/storage.go
@@ -1,6 +1,10 @@
 package core
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 const CurrentRev = -1
 
@@ -29,6 +33,33 @@ type Storage interface {
 	PutPage(path string, page *Page) error
 	DirList(path string) []string
 	GetPageList(root string) []string
+}
+
+func pagelistProc(root string, pagelist []string) ([]string, []string) {
+	dirs := []string{}
+	pages := []string{}
+
+	rootLen := len(strings.Split(root, "/")) - 1
+	println(rootLen)
+
+	for _, page := range pagelist {
+		if !strings.HasPrefix(page, root) {
+			continue
+		}
+		els := strings.Split(page, "/")[1:]
+		fmt.Printf("%v\n", els)
+
+		if len(els) == rootLen {
+			pages = append(pages, root+els[len(els)-1])
+		} else {
+			e := root + els[rootLen-1]
+			if len(dirs) == 0 || dirs[len(dirs)-1] != e {
+				dirs = append(dirs, e)
+			}
+		}
+	}
+
+	return dirs, pages
 }
 
 /*
