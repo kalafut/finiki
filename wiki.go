5241cdb4403e99e6debd06c2d6ed2d8e4d1f3bfd
Jim Kalafut <jim@kalafut.net>
2016-11-20 00:16:24 -0800

    Make / required for dirs

--- wiki.go
+++ wiki.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // Wiki represents the entire Wiki, contains the db
 type Wiki struct {
@@ -25,16 +28,21 @@ func (wiki Wiki) Route(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	action := queryValues.Get("action")
 
+	isDir := len(wiki.store.GetPageList(path)) > 0 || len(wiki.store.DirList(path)) > 0
+
+	if isDir && !strings.HasSuffix(path, "/") {
+		http.Redirect(w, r, path+"/", http.StatusSeeOther)
+		return
+	}
+
 	switch {
 	case r.PostFormValue("update") == "update":
 		wiki.Update(w, r)
 	case action == "edit":
 		wiki.Edit(w, r)
+	case isDir:
+		wiki.Dir(w, r)
 	default:
-		if len(wiki.store.GetPageList(path)) > 0 || len(wiki.store.DirList(path)) > 0 {
-			wiki.Dir(w, r)
-		} else {
-			wiki.Show(w, r)
-		}
+		wiki.Show(w, r)
 	}
 }
