d08d41adc43d8bdc6b64e56374f8517ca4c557da
Jim Kalafut <jim@kalafut.net>
2016-12-29 16:02:39 -0800

    Add markdown rendering

--- finiki/views.py
+++ finiki/views.py
@@ -3,17 +3,25 @@ from os.path import join
 from finiki import app
 from flask import redirect, render_template, request
 
+import mistune
+
+markdown = mistune.Markdown()
+
 ROOT = '/Users/kalafut/Dropbox/finiki'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
     isdir = os.path.isdir(join(ROOT, path))
-    print(isdir)
-    print(path)
     if isdir and not path.endswith('/') and path != '':
         return redirect(path + '/'), 303
-    return render_template('dir.html', dirs=dirs(path), pages=pages(path))
+
+    if isdir:
+        return render_template('dir.html', dirs=dirs(path), pages=pages(path))
+
+    with open(join(ROOT, path)) as f:
+        contents = f.read()
+        return render_template('show.html', text=markdown(contents), Page='')
 
 def dirs(path='.'):
     for entry in os.scandir(join(ROOT, path)):
