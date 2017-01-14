df0d3c117b2678b3622ea05e596128de2a346b78
Jim Kalafut <jim@kalafut.net>
2017-01-13 22:34:22 -0800

    Delete support

--- finiki/views.py
+++ finiki/views.py
@@ -10,11 +10,20 @@ markdown = mistune.Markdown()
 ROOT = '/Users/kalafut/Dropbox/finiki'
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/<path:path>', methods=['GET', 'POST'])
 def index(path):
     isdir = os.path.isdir(join(ROOT, path))
     if isdir and not path.endswith('/') and path != '':
         return redirect(path + '/'), 303
+
+    if request.method == 'POST':
+        if 'delete' in request.form:
+            os.remove(join(ROOT, path))
+            return redirect('/')
+        else:
+            with open(join(ROOT, path), 'w') as f:
+                f.write(request.form['text'])
+                return redirect(path)
 
     if isdir:
         return render_template('dir.html', dirs=dirs(path), pages=pages(path))
@@ -23,7 +32,9 @@ def index(path):
     if action == 'edit':
         with open(join(ROOT, path)) as f:
             contents = f.read()
-            return render_template('edit.html', text=contents)
+            return render_template('edit.html', text=contents, path=path)
+    elif action == 'delete':
+        return render_template('delete.html', path=path)
 
     with open(join(ROOT, path)) as f:
         contents = f.read()
